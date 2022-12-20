7b04f34ba24ca7927acbcc12f680f228ffc23dfb
David Perelman <dperelman3@gmail.com>
2022-12-20 19:05:24 +0200

    setup 'popover responds to hover' test

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -30,3 +30,27 @@ test('Checkbox enabled on first click and disabled on second click', async () =>
   await user.click(checkbox);
   expect(confirmButton).toBeDisabled();
 });
+
+test('popover responds to hover', async () => {
+  const user = userEvent.setup();
+
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears on mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
+});
