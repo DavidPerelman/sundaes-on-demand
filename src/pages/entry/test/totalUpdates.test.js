b8ee6419bbd3235138af8bcfea9ec920a39022bb
David Perelman <dperelman3@gmail.com>
2022-12-21 11:38:36 +0200

    setup 'update scoop subtotal when scoops change' test

---
+++ src/pages/entry/test/totalUpdates.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Options from '../Options';
+
+test('update scoop subtotal when scoops change', async () => {
+  const user = userEvent.setup();
+  render(<Options optionType='scoops' />);
+
+  //  make sure total starts out $0.00
+  const scoopSubtotal = screen.getByText('Scoops total: $', { exact: false });
+  expect(scoopSubtotal).toHaveTextContent('0.00');
+
+  // update vanilla scoops to 1 and check the subtotal
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla',
+  });
+
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, '1');
+
+  expect(scoopSubtotal).toHaveTextContent('2.00');
+
+  // update chocolate scoops to 2 and check the subtotal
+  const chocolateTotal = await screen.findByRole('spinbutton', {
+    name: 'Chocolate',
+  });
+
+  await user.clear(chocolateTotal);
+  await user.type(chocolateTotal, '2');
+
+  expect(scoopSubtotal).toHaveTextContent('6.00');
+});
